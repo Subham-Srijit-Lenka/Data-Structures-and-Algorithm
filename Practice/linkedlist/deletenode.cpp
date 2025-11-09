1e2585be565d61698461c5f8fcee3197517ce183
Subham srijit Lenka <subhamsrijit19@gmail.com>
2025-11-09 12:46:06 +0530

    deleting node

---
+++ Practice/linkedlist/deletenode.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// solution
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+// helper
+void printList(ListNode *head)
+{
+    while (head != NULL)
+    {
+        cout << head->val;
+        if (head->next)
+            cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ListNode *head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+
+    cout << "Original list: ";
+    printList(head);
+
+    ListNode *nodeToDelete = head->next;
+
+    Solution sol;
+    sol.deleteNode(nodeToDelete);
+
+    cout << "After deleting node 5: ";
+    printList(head);
+
+    return 0;
+}
